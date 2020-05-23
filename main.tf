abac3a67c1d3efb243443535f208b86fb30aab44
Tamas Sallai <gsashee@gmail.com>
2020-05-23 11:17:53 +0200

    initial

---
+++ main.tf
@@ -0,0 +1,191 @@
+provider "aws" {
+}
+
+# lambda + api gw
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "/tmp/lambda.zip"
+  source {
+    content  = <<EOF
+module.exports.handler = async (event, context) => {
+	return event;
+};
+EOF
+    filename = "main.js"
+  }
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "${random_id.id.hex}-function"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  handler = "main.handler"
+  runtime = "nodejs12.x"
+  role    = aws_iam_role.lambda_exec.arn
+}
+
+data "aws_iam_policy_document" "lambda_exec_role_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_role_policy" "lambda_exec_role" {
+  role   = aws_iam_role.lambda_exec.id
+  policy = data.aws_iam_policy_document.lambda_exec_role_policy.json
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# api gw
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "api-${random_id.id.hex}"
+  protocol_type = "HTTP"
+  target        = aws_lambda_function.lambda.arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+# api gw with path key
+resource "aws_apigatewayv2_api" "api-path" {
+  name          = "api-path-${random_id.id.hex}"
+  protocol_type = "HTTP"
+  target        = aws_lambda_function.lambda.arn
+	route_key = "ANY /test/{proxy+}"
+}
+
+resource "aws_lambda_permission" "apigw-path" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api-path.execution_arn}/*/*"
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = replace(aws_apigatewayv2_api.api.api_endpoint, "/^https?://([^/]*).*/", "$1")
+    origin_id   = "apigw_root"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+  origin {
+    domain_name = replace(aws_apigatewayv2_api.api-path.api_endpoint, "/^https?://([^/]*).*/", "$1")
+    origin_id   = "apigw_path"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled = true
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "apigw_root"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "/test/*"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "apigw_path"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+output "domain" {
+  value = aws_cloudfront_distribution.distribution.domain_name
+}
+
+output "api2" {
+  value = aws_apigatewayv2_api.api-path.api_endpoint
+}
+
+output "api" {
+  value = aws_apigatewayv2_api.api.api_endpoint
+}
+
